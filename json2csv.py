c43bd232d3b75906d1e3be2cb934735c95772fa2
Daniel Izquierdo <dizquierdo@bitergia.com>
2017-09-29 13:13:34 +0200

    Update copyright info

--- json2csv.py
+++ json2csv.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2017 Bitergia
-#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3 of the License, or
